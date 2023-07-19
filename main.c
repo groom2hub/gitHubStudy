e7387565fb23eb18ad195a95aa0f9bb3ebc71191
groom2hub <groomwnsgur35@gmail.com>
2023-07-19 16:33:09 +0900

    Feat: multiplication

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+void multiplication();
+
 int main(){
-	printf("Hello World!");
+	printf("Hello World!\n");
+
+	multiplication();
+
 	return 0;
+}
+
+void multiplication(){
+	for(int i = 0; i < 10; i++) {
+		for(int j = 0; j < 10; j++) {
+			printf("%d * %d = %d", i, j, i*j);
+		}
+	}
 }
